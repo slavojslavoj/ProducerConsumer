24e27dcd6213786490fed09dd44f7531e3af212a
slavojslavoj <69841777+slavojslavoj@users.noreply.github.com>
2021-07-28 10:33:31 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,25 +5,19 @@ public class Test {
     public static void main(String[] args) throws InterruptedException {
         ProducerConsumer pc = new ProducerConsumer();
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.produce();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Thread thread1 = new Thread(() -> {
+            try {
+                pc.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Thread thread2 = new Thread(() -> {
+            try {
+                pc.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
 
